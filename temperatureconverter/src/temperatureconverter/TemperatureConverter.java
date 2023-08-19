96bdb83710d0dd326b16f81330229747df25c0e6
Sharathjmandakki <sharathjmandakki@gmail.com>
2023-08-19 17:02:16 +0530

    EXTRA

--- temperatureconverter/src/temperatureconverter/TemperatureConverter.java
+++
@@ -1,8 +0,0 @@
-package temperatureconverter;
-
-public class TemperatureConverter {
-public double convertFahrenheitToCelsius(double fahenheit) 
-{
-	return ((fahenheit-32)*5)/9;//whole / by 9
-}
-}
